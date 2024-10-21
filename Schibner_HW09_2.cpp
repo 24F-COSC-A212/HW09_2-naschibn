9e408f8b202c218c4a767fed9e12071b607d014b
naschibn <157063022+naschibn@users.noreply.github.com>
2024-10-20 20:02:22 -0500

    Schibner_HW09_2

---
+++ Schibner_HW09_2.cpp
@@ -0,0 +1,40 @@
+//Line Numbers 
+//To format the info in the file
+//Nalani Schibner 
+//October 17th, 2024
+
+#include <iostream>
+#include <iomanip>
+#include <fstream> 
+
+using namespace std; 
+
+int main()
+{
+    ifstream inFile;
+    char inName[100];
+    string inputLine = " "; 
+    int lineNum = 1;
+
+    cout << "Enter the input file name: ";
+    cin >> inName;
+
+    inFile.open(inName);
+
+    if (!inFile)
+    {
+        cout << "Error opening file " << inName << endl;
+        return 666; 
+    } 
+
+    while (!inFile.eof())
+    {
+        getline(inFile, inputLine, '\n');
+        cout << setw(4) << right << lineNum << ": " << inputLine << endl; 
+        lineNum++;
+    }
+
+    inFile.close();
+    
+    return 0; 
+}
